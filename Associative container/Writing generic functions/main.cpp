23b2dc025f6c4329b34731bf5d0f7e8df2c3c088
Mihail <mihai.kamydzy@gmail.com>
2025-03-09 16:53:17 +0200

    capture 8

--- Associative container/Writing generic functions/main.cpp
+++ Associative container/Writing generic functions/main.cpp
@@ -2,40 +2,103 @@
 #include <string>
 #include <map>
 #include <iostream>
-#include <stdexcept>  // Добавили для std::domain_error
-#include <algorithm>  // Добавили для std::sort
+#include <stdexcept>  
+#include <algorithm>  
 #include <iterator>
+#include <ios>
+#include <istream>
+#include <list>
+bool space(char c) {
+    return isspace(c);
+}
 
-template <class Ran , class X> 
-bool binar_search(Ran begin, Ran end, const X& x) 
+// Функция для проверки, не является ли символ пробелом
+bool not_space(char c) {
+    return !isspace(c);
+}
+template <class Out>
+void split(const std::string& str, Out os)
 {
-	while (begin < end) {
-		Ran mid = begin + (end - begin) / 2;
+    typedef std::string::const_iterator iter;
 
-		if (x < *mid) {
-			end = mid;
-		}
-		else if (*mid < x) {
-			begin = mid + 1;
-		}
-		else return true;
-	}
-	return false;
-	
+    iter i = str.begin();
+    while (i != str.end()) {
+        i = find_if(i, str.end(), not_space);
 
+        iter j = find_if(i, str.end(), space);
+        std::cout << "i: " << std::distance(str.begin(), i) << ", j: " << std::distance(str.begin(), j) << std::endl;
+        std::cout << "Word: " << std::string(i, j) << std::endl;
+
+        if (i != str.end()) {
+            *os++ = std::string(i, j);
+        }
+        i = j;
+    }
 }
-int main() {
-	std::vector<int> numbers = { 1,2,3,4,5,6,7,8,9,10 };
-	int target = 0;
-	/*binar_search(numbers.begin(), numbers.end(), target);*/
-	if (binar_search(numbers.begin(), numbers.end(), target)) {
-		std::cout<<"Found: "<< target <<std::endl;
-	}
-	else {
-		std::cout << "Not Found: " << target << std::endl;
-	}
-	return 0;
+int main()
+{
+    std::list<std::string> word_list;
+    std::string s;
+    while (getline(std::cin, s)) {
+        split(s, back_inserter(word_list));
+        split(s, std::ostream_iterator<std::string>(std::cout, ","));
+
+    }
+    for (auto it = word_list.begin();it != word_list.end();it++) {
+        std::cout << *it << "This are For: " << std::endl;
+    }
+    return 0;
 }
+
+//template<class It,class X>
+//X my_copy(It begin, It end, X x) {
+//	
+//	while (begin != end) {
+//		x = *begin;
+//		++begin;
+//	}
+//	return x;
+//}
+//int main()
+//{
+//	std::vector<int>v;
+//	my_copy(std::istream_iterator<int>(std::cin), std::istream_iterator<int>(), back_inserter(v));
+//	//for (auto it = v.begin(); it != v.end();it++) {
+//	//	std::cout << "Numbers: " << *it << std::endl;
+//	//}
+//	my_copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, ","));
+//	return 0;
+//}
+
+// Binary search!!¬!
+//template <class Ran , class X> 
+//bool binar_search(Ran begin, Ran end, const X& x)
+//{
+//	while (begin < end) {
+//		Ran mid = begin + (end - begin) / 2;
+//
+//		if (x < *mid) {
+//			end = mid;
+//		}
+//		else if (*mid < x) {
+//			begin = mid + 1;
+//		}
+//		else return true;
+//	}
+//	return false;
+//}
+//int main() {
+//	std::vector<int> numbers = { 1,2,3,4,5,6,7,8,9,10 };
+//	int target = 0;
+//	/*binar_search(numbers.begin(), numbers.end(), target);*/
+//	if (binar_search(numbers.begin(), numbers.end(), target)) {
+//		std::cout<<"Found: "<< target <<std::endl;
+//	}
+//	else {
+//		std::cout << "Not Found: " << target << std::endl;
+//	}
+//	return 0;
+//}
 //template <class Bi> void my_reverse(Bi begin, Bi end)
 //{
 //	while (begin != end) {
